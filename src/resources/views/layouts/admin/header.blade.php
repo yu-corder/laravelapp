9f92c28aca3290e4098f6ad95c799b51f10775eb
yu-corder <yuuya.9602@icloud.com>
2026-04-02 17:03:31 +0900

    Style: Add icons to the header navigation

--- src/resources/views/layouts/admin/header.blade.php
+++ src/resources/views/layouts/admin/header.blade.php
@@ -2,10 +2,9 @@
 <header class="header">
     <div class="header-inner">
         <div class="logo">
-            <h1>
-                <small>サウナラリー管理</small>
-                Sauna_rally!
-            </h1>
+            <a href="{{ url('admin/sauna') }}">
+                <img class="sauna-rally-logo" src="{{ asset('images/icons/sauna_rally_log3.png') }}" alt="Sauna_rally!">
+            </a>
         </div>
         <nav class="login-user">
             <a href="{{ url('profile') }}">
--- src/resources/views/layouts/sauna/header.blade.php
+++ src/resources/views/layouts/sauna/header.blade.php
@@ -2,10 +2,9 @@
 <header class="header">
     <div class="header-inner">
         <div class="logo">
-            <h1>
-                <small>サウナラリー</small>
-                Sauna_rally!
-            </h1>
+            <a href="{{ url('saunas/') }}">
+                <img class="sauna-rally-logo" src="{{ asset('images/icons/sauna_rally_log3.png') }}" alt="Sauna_rally!">
+            </a>
         </div>
     </div>
 </header>
