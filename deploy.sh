91ab69f97c8b27d1539033bb8dd74890891fc8c6
yu-corder <yuuya.9602@icloud.com>
2026-04-05 18:33:46 +0900

    Chore: optimize deploy.sh for production environment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 git pull origin main
-docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web composer install
+
+docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+
+docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web composer install --no-dev --optimize-autoloader
 docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web npm install
 docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web npm run build
+
 docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web php artisan migrate --force
 docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web php artisan config:cache
+docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web php artisan route:cache
+docker compose -f docker-compose.yml -f docker-compose.prod.yml exec web php artisan view:cache
