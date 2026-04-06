57c54fff83c451e53e793fcb2756f80ba4de9d5c
yu-corder <yuuya.9602@icloud.com>
2026-04-06 23:21:20 +0900

    Fix: update nginx config and ssl renew script for sauna-rally

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-docker run --rm -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -v "$(pwd)/src:/var/www" certbot/certbot renew --webroot -w /var/www/public
+
+cd /var/www/sauna-rally
+
+docker run --rm \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -v "/var/log/letsencrypt:/var/log/letsencrypt" \
+  -v "/var/www/sauna-rally/src:/var/www" \
+  certbot/certbot renew --webroot -w /var/www --no-random-sleep-on-renew
 
 docker exec saunas-nginx nginx -s reload
